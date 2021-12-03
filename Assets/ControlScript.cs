451cb5c650190128e66833d37d6bae2f1b7f409c
GameGuyy <87716495+GameGuyy@users.noreply.github.com>
2021-12-03 13:38:26 +0530

    update 0.1

--- Assets/ControlScript.cs
+++ Assets/ControlScript.cs
@@ -10,14 +10,18 @@ public class ControlScript : MonoBehaviour
     public float speed = 5f;
     Vector3 lookAtTarget;
     Quaternion playerRot;
+    public Transform player;
+    
+    public float miny, maxy,fixedX;
+    //public Animation anim;
     private void Awake()//
     {
         instance = this;
     }
     void Start()
     {
-        
-        
+        //anim = gameObject.GetComponent<Animation>();
+
     }
 
     void Update()
@@ -26,6 +30,10 @@ public class ControlScript : MonoBehaviour
         if(Input.GetMouseButton(0))
         {
             SetTargetPosition();
+        }
+        else
+        {
+            isMoving = false;
         }
         if(isMoving)
         {
@@ -54,9 +62,12 @@ public class ControlScript : MonoBehaviour
     void Move()
     {
         //transform.rotation = Quaternion.Slerp(transform.rotation, playerRot, speed * Time.deltaTime);
-
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-        if(transform.position == targetPosition)
+        
+        Vector3 rot = Vector3.MoveTowards(player.position, targetPosition, speed * Time.deltaTime);
+        rot.y = Mathf.Clamp(rot.y, miny, maxy);
+        rot.x = fixedX;
+        player.position = rot;
+        if (player.position == targetPosition)
         {
             isMoving = false;
         }
